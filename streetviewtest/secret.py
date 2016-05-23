b22991fe7ba1c0c9c5458779ffd669e4aa17bf40
Holdenmt11 <holdenmt11@gmail.com>
2016-05-23 16:57:42 -0400

    Delete secret.py

--- streetviewtest/secret.py
+++
@@ -1,7 +0,0 @@
-streetview = "AIzaSyCHWf23o1OLkFYmczLt45Cj-bexnOK_G0k"
-
-
-consumer_key = 	"NSRHB3rJnUUX6huC0AJVGQ6HR"
-consumer_secret = "6O4KX6DsC96DqevZeKdDzBXeGamydNmden2LkZaIM2856vuomj"
-key = "733396475296645120-AipBOyuwlEXl4LjJFdViwuO91Vo00lO"
-secret = "aSgWx9jATQ6nDOj1Gd91gXogryZx70l43WYPu9sVf6rB6"
